ced4f93ca6cc352aa8f4571fa1ff2192a4c1c54a
Shub1970 <18shubham1998@gmail.com>
2023-01-11 20:22:21 +0530

    minute is working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 function App() {
-  const [second, setSecond] = useState(60);
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(25);
   const [isrunning, setIsrunning] = useState(false);
   const [inter, setInter] = useState();
   useEffect(() => {
     if (isrunning === true) {
       const i = setInterval(() => {
-        setSecond((previous) => setSecond(previous - 1));
-      }, 1000);
+        setSecond((previous) => {
+          return (previous = previous - 1);
+        });
+      }, 100);
       setInter(i);
     }
     if (isrunning === false) {
       clearInterval(inter);
     }
   }, [isrunning]);
+  useEffect(() => {
+    if (second == 0) {
+      setMinute((prev) => prev - 1);
+      setSecond(60);
+    }
+  }, [second, minute]);
   return (
     <>
       <div className="time-block">
         <div className="second">{second}</div>
-        <div className="minute"></div>
+        <div className="minute">{minute}</div>
       </div>
       <div>
         <button className="start" onClick={() => setIsrunning(true)}>
